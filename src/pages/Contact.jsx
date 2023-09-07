119a6569058d247ebc22ae194e0c991d659b583a
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-09-07 04:22:22 -0600

    contact_me-page-finished

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,28 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_calcantara",
+        "template_calcantara",
+        form.current,
+        "mx0g-rCY9I2xw_V7z"
+      )
+      .then(
+        (result) => {
+          alert("message ssent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <article className="min-h-full relative px-4 py-6">
       <h1 className="font-poppins font-semibold text-3xl py-10 text-center">
@@ -6,18 +30,41 @@ const Contact = () => {
       </h1>
       <section className="flex justify-center relative z-10">
         <div className="flex flex-col justify-center  w-[400px] h-[500px] sm:h-[800px] sm:w-[900px] bg-gray-600/40 px-6 py-9 rounded-3xl">
-          <form className="font-sen capitalize">
+          <form ref={form} onSubmit={sendEmail} className="font-sen capitalize">
             <div>
-              <h3>Name</h3>
-              <input type="text" className=" bg-purple rounded-lg w-full" />
+              <label>Name</label>
+              <input
+                type="text"
+                name="user_name"
+                className=" bg-purple rounded-lg w-full"
+                required
+              />
             </div>
             <div>
-              <h3>email</h3>
-              <input type="email" className=" bg-purple rounded-lg w-full" />
+              <label>email</label>
+              <input
+                type="email"
+                name="user_email"
+                className=" bg-purple rounded-lg w-full"
+                required
+              />
             </div>
             <div>
-              <h3>message</h3>
-              <input type="text" className="h-28 bg-purple rounded-lg w-full" />
+              <label>message</label>
+              <textarea
+                type="text"
+                name="message"
+                className="h-28 bg-purple rounded-lg w-full"
+                required
+              />
+            </div>
+            <div className="pt-4">
+              <button
+                type="submit"
+                className="bg-purple hover:bg-[rgb(92,92,184)] w-full py-2 rounded-md"
+              >
+                send
+              </button>
             </div>
           </form>
         </div>
@@ -33,3 +80,5 @@ const Contact = () => {
   );
 };
 export default Contact;
+
+<input type="submit" value="Send" className="cursor-pointer" />;
