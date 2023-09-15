1f8eabeae2bc8d63aa612dc4a28e15e147b42570
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-09-15 17:28:01 -0600

    resume-updated

--- src/pages/Technologies.jsx
+++ src/pages/Technologies.jsx
@@ -4,7 +4,7 @@ const Technologies = () => {
       <div className="max-w-max  px-10 py-3 mx-auto text-center border text-4xl rounded-full font-poppins font-semibold cursor-pointer bg-white/50 hover:bg-gray-100/40 transition-color hover:scale-110 duration-150 delay-200 transform">
         <a
           className="flex items-center justify-center gap-3"
-          href="/docs/resume.pdf"
+          href="/docs/CarlosResume.pdf"
           target="_blank"
         >
           <i class="bx bxs-download animate-bounce"></i>
