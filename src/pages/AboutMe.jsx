ef458f455705246ad448552bbd4741afac5f9717
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-09-15 07:12:09 -0600

    Adding-download-button

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,14 +1,12 @@
 const AboutMe = () => {
   return (
-    <article id="about" className="min-h-full relative px-4 py-6">
+    <article className="relative px-4 py-6">
       <h1 className="font-poppins font-semibold text-4xl py-20 px-10">
         About me
       </h1>
-      <section className="flex justify-center items-center relative z-10 left-0 md:left-1/4">
+      <section className="grid justify-center items-center relative z-10 left-0 md:left-1/4">
         <div className="grid items-center h-[800px] w-[90%] sm:w-[70%] md:w-[370px] lg:w-1/2 xl:w-1/2 2xl:w-1/2 bg-gray-600/40 px-6 py-8 rounded-3xl gap-5 scroll-smooth">
-          <h2 className="font-poppins font-semibold text-5xl py-10">
-            Hi!
-          </h2>
+          <h2 className="font-poppins font-semibold text-5xl py-10">Hi!</h2>
           <p className="font-sen text-p-gray text-lg font-normal pl-3">
             I'm Carlos, a Computer Engineering student at UAEMex, also attending
             Academlo boot camp. I'm a quick learner who thrives in fast-paced
@@ -30,6 +28,7 @@ const AboutMe = () => {
           </div>
         </div>
       </section>
+
       <section>
         <img
           className="absolute -left-[55%] bottom-8 md:-left-1/2 md:-bottom-[10px] lg:-left-8"
--- src/pages/Technologies.jsx
+++ src/pages/Technologies.jsx
@@ -1,40 +1,50 @@
 const Technologies = () => {
   return (
     <section className="w-full px-6 py-6 mx-auto">
+      <div className="max-w-max  px-10 py-3 mx-auto text-center border text-4xl rounded-full font-poppins font-semibold cursor-pointer bg-white/50 hover:bg-gray-100/40 transition-color hover:scale-110 duration-150 delay-200 transform">
+        <a
+          className="flex items-center justify-center gap-3"
+          href="/docs/resume.pdf"
+          target="_blank"
+        >
+          <i class="bx bxs-download animate-bounce"></i>
+          <p>Resume</p>
+        </a>
+      </div>
       <h1 className="font-poppins px-10 py-20 text-4xl font-semibold">
         Technologies
       </h1>
       <div className="bg-gray-600/40 rounded-3xl text-8xl flex flex-wrap items-center justify-center gap-5 px-6 py-8">
         <span className="py-2">
-          <i className="bx bxl-html5 hover:bg-gray-300/40 transition-color  hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">HTML</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-css3 bx hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-css3 hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">CSS</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-javascript bx hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-javascript hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">JavaScript</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-react bx hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-react hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">React</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-tailwind-css bx hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-tailwind-css hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">Tailwind</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-git bx hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-git hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">Git</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-github bx hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-github hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">GitHub</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-netlify bx hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-netlify hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">Netlify</h3>
         </span>
       </div>
