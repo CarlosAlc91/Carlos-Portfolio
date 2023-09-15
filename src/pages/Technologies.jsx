82b59538c907f8e26bb581f74d5116929a678083
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-09-15 04:35:46 -0600

    fixing-technologies-component

--- src/pages/Technologies.jsx
+++ src/pages/Technologies.jsx
@@ -6,35 +6,35 @@ const Technologies = () => {
       </h1>
       <div className="bg-gray-600/40 rounded-3xl text-8xl flex flex-wrap items-center justify-center gap-5 px-6 py-8">
         <span className="py-2">
-          <i className="bx bxl-html5 hover:bg-gray-300/40 transition-color  hover:scale-150 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-html5 hover:bg-gray-300/40 transition-color  hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">HTML</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-css3 bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-150 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-css3 bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">CSS</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-javascript bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-150 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-javascript bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">JavaScript</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-react bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-150 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-react bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">React</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-tailwind-css bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-150 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-tailwind-css bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">Tailwind</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-git bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-150 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-git bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">Git</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-github bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-150 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-github bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">GitHub</h3>
         </span>
         <span className="py-2">
-          <i className="bx bxl-netlify bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-150 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
+          <i className="bx bxl-netlify bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-lg text-center">Netlify</h3>
         </span>
       </div>
