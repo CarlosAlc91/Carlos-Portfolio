6cc14635e540efcba59b58aa13b349dc97001cc4
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-09-16 06:51:33 -0600

    working-on-navbar

--- src/components/layout/HeaderNavbar.jsx
+++ src/components/layout/HeaderNavbar.jsx
@@ -11,17 +11,17 @@ const HeaderNavbar = () => {
   ];
 
   return (
-    <div className="shadow-md w-full bg-white text-red fixed top-0 left-0">
+    <div className="shadow-md w-full bg-white text-red fixed top-0 left-0 z-50">
       <div className="md:px-10 px-7 md:flex justify-between items-center">
         {/* logo */}
-        <div className="flex text-2xl cursor-pointer items-center gap-2">
+        <div className="flex text-2xl cursor-pointer items-center gap-2 z-50">
           <img src="/images/logo.png" alt="" />
           <span className="font-bold">Carlos</span>
         </div>
         {/* menu icon */}
         <div
           onClick={() => setIsOpen(!isOpen)}
-          className="w-7 h-7 text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+          className="text-4xl absolute right-8 top-6 cursor-pointer md:hidden"
         >
           {isOpen ? (
             <i className="bx bx-menu "></i>
@@ -32,12 +32,12 @@ const HeaderNavbar = () => {
 
         {/* links */}
         <ul
-          className={`md:flex pl-9 md:pl-0 md:items-center md:pb-0 pb-12 absolute md:z-auto z-[-1] right-0 w-full transition-all bg-white duration-500 ease-in ${
-            isOpen ? "visible" : "hidden"
+          className={`md:flex md:justify-end pl-9 md:pl-0 md:items-center md:pb-0 pb-12 absolute md:z-auto z-[-1] right-0 w-full transition-all bg-white duration-500 ease-in ${
+            isOpen ? "hidden" : "visible"
           }`}
         >
           {Links.map((link) => (
-            <li className="my-7 md:my-0 md:ml-8 font-semibold">
+            <li className="my-4 md:my-0 md:ml-8 font-semibold">
               <a href="/">{link.name}</a>
             </li>
           ))}
