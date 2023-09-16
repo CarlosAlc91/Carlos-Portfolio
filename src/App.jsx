c08678238ec72ac2c542248be05ba9cff450217f
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-09-16 15:13:19 -0600

    working-on-hero

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,10 @@ function App() {
   return (
     <main className="min-h-screen overflow-x-hidden overflow-y-hidden bg-bg-main text-white">
       <HeaderNavbar />
-
       <AboutMe />
       <Technologies />
       <Projects />
       <Contact />
-
       <WhatsAppButton />
     </main>
   );
