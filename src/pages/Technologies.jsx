25f51494eec809ff73438a027c67bf8fa09aeba4
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-09-21 16:24:26 -0600

    fixing-responsiveness

--- src/pages/Technologies.jsx
+++ src/pages/Technologies.jsx
@@ -4,7 +4,7 @@ const Technologies = () => {
       <h1 className="font-poppins px-10 py-20 text-4xl font-semibold md:text-6xl ">
         Technologies
       </h1>
-      <div className="flex flex-wrap items-center justify-center gap-7 bg-gray-600/40 rounded-3xl text-7xl md:text-9xl px-6 py-8 md:w-1/2 mx-auto animate-animateUser">
+      <div className="flex flex-wrap items-center justify-center gap-7 bg-gray-600/40 rounded-3xl text-7xl md:text-8xl px-6 py-8 md:w-1/2 mx-auto">
         <span className="py-2">
           <i className="bx bxl-html5 hover:bg-gray-300/40 transition-color hover:scale-110 px-1 py-1 duration-150 delay-200 transform rounded-full"></i>
           <h3 className="text-3xl text-center">HTML</h3>
