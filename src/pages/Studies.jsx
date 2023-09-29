d76766ae72f71420bbfb4d989ad6db3e1d46a089
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-09-29 02:50:54 -0600

    Academic-section-fixed-and-finished

--- src/pages/Studies.jsx
+++ src/pages/Studies.jsx
@@ -12,7 +12,7 @@ const Studies = () => {
             src="/images/uaem.jpeg"
             alt=""
           />
-          <h3 className="font-sen font-semibold text-lg text-center">
+          <h3 className="font-sen font-semibold text-md text-center">
             UAEMex (Universidad Autonoma del Estado de Mexico)
           </h3>
           <h4 className="font-poppins text-lg text-center text-[#e0e0e0]">
@@ -23,7 +23,7 @@ const Studies = () => {
           </p>
         </div>
         {/* academlo */}
-        <div className=" flex flex-col h-[300px] w-[250px] justify-center items-center gap-4 border bg-container hover:bg-[#007997] text-white py-3 px-2 rounded-xl mx-auto transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-500">
+        <div className=" flex flex-col h-[300px] w-[250px] justify-center items-center gap-4 border bg-container hover:bg-[#007997] text-white py-3 px-2 rounded-xl mx-auto transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-500 scro">
           <img
             className="h-[90px] w-[90px] rounded-full"
             src="/images/academlo.jpg"
@@ -55,6 +55,13 @@ const Studies = () => {
           <p className="font-poppins text-lg text-center text-[#e0e0e0]">
             React.js
           </p>
+          <a
+            className="w-1/2 px-4 mx-auto py-2 inline-block bg-[#282C34] hover:bg-[#FF4081] transition-colors rounded-lg text-center"
+            href="https://drive.google.com/file/d/19nYu-tfagdrJyPXmSDrMsjlF97ww5hBW/view?usp=sharing"
+            target="_blank"
+          >
+            Certificate
+          </a>
         </div>
         {/* academlo */}
         <div className=" flex flex-col h-[300px] w-[250px] justify-center items-center gap-4 border bg-container hover:bg-[#007997] text-white py-3 px-2 rounded-xl mx-auto transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-500">
@@ -72,6 +79,13 @@ const Studies = () => {
           <p className="font-poppins text-lg text-center text-[#e0e0e0]">
             Fundamentals
           </p>
+          <a
+            className="w-1/2 px-4 mx-auto py-2 inline-block bg-[#282C34] hover:bg-[#FF4081] transition-colors rounded-lg text-center"
+            href="https://drive.google.com/file/d/127lAxuQ0dUKY0ylgTgbTpqyzkp9PuLnc/view?usp=sharing"
+            target="_blank"
+          >
+            Certificate
+          </a>
         </div>
         {/* hcc */}
         <div className=" flex flex-col h-[300px] w-[250px] justify-center items-center gap-4 border bg-container hover:bg-[#007997] text-white py-3 px-2 rounded-xl mx-auto transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-500">
